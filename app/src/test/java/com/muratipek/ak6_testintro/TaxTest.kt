77a7ecec49026c83e8609341f1a470981b06adbe
Murat03 <muratipekk033@gmail.com>
2023-02-24 18:10:21 +0300

    Before - After testing added

--- app/src/test/java/com/muratipek/ak6_testintro/TaxTest.kt
+++ app/src/test/java/com/muratipek/ak6_testintro/TaxTest.kt
@@ -1,17 +1,29 @@
 package com.muratipek.ak6_testintro
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class TaxTest {
+    private lateinit var tax : Tax
+    @Before
+    fun setup(){
+        tax = Tax()
+    }
+
+    @After
+    fun teardown(){
+        
+    }
+
+    @Test
     fun calculateTaxTest(){
-        val tax = Tax()
         val netTax = tax.calculateTax(100.0, 0.1)
         assertThat(netTax).isEqualTo(10)
     }
     @Test
     fun calculateIncomeTest(){
-        val tax = Tax()
         val netIncome = tax.calculateIncome(100.0, 0.1)
         assertThat(netIncome).isEqualTo(90)
     }
